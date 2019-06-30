865f8ce4b411083a7b47dfa4adea88c3cc5d2576
Marc Maceira <mmaceira.zayas@gmail.com>
2019-06-30 14:55:17 -0400

    Functions
    Try it out: https://www.hackerrank.com/challenges/c-tutorial-functions/problem

---
+++ Functions/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int max_of_four(int a, int b, int c, int d) {
+	int max = 0, arr[] { a, b, c, d};
+	
+	for (int i = 0; i < 4; i++) {
+		if (max < arr[i])
+			max = arr[i];
+	}
+	
+	return max;
+}
+
+int main() {
+	int a, b, c, d;
+	scanf("%d %d %d %d", &a, &b, &c, &d);
+	int ans = max_of_four(a, b, c, d);
+	printf("%d", ans);
+	
+	return 0;
+}
